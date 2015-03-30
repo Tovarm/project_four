cfaf5f0b15ee64a94aa6c4ab2105c19181504e46
Tovarm <tovamosk@gmail.com>
2015-03-30 13:56:30 -0400

    many changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -17,6 +17,9 @@ var contestant_id = $('#contestant_id').text();
 console.log("CONTESTANT ID IS " + contestant_id);
 var username = $('#username').text();
 console.log("USERNAME IS " + username);
+var chances = 1
+
+
 
 // enter key can be used to submit answer
 $('.user_answer').keypress(function (e) {
@@ -29,72 +32,75 @@ $('.user_answer').keypress(function (e) {
 });   
 
 console.log("round is currently:" + $which_round.text());
-
-$('#reveal_answer').hide();
-$('.hidden_answer').hide();
-$('.individual_question').hide();
+//hide these divs by default. New game button only shows up when the current game ends
+// $('.hint_button').hide();
 // $('#new_game').hide();
-$('.question_value').hide();
+// $('#reveal_answer').hide();
+$('.hidden_answer').hide();
+// $('.dashes').hide();
+//hide each question so that they appear only when modal is called 
+$('.individual_question').hide();
+// $('.question_value').hide();
   
+
+// iclose button is clicked, reveal correct answer inside reveal_answer div and unhide. Also add class of already_answered- anything with that class has its modal button disabled
+
+$('.whole_question').on("click", ".give_up_close", function () {
+  chances = 1;
+  $('.modal-body').append($('.dashes'));
+  console.log("close button clicked");
+  var $hidden_answer = $(this).parents('.modal-body').children('.input').children('.hidden_answer').text();
+  if($(this).hasClass('already_answered')){
+  $('#reveal_answer').hide();
+} else {
+  // if contestant gives up, add class reveal_answer_wrong to apply styling 
+  $('#reveal_answer').append('<p> Too bad. The right answer was "' + $hidden_answer.toUpperCase() + '".</p>').addClass('reveal_answer_wrong');
+  $('#reveal_answer').show();
+  chances--;
+  $(this).text("Are you sure?");
+  }
+});
+
+//upon clicking on any of the buttons, modal will launch. The correct answer is hidden in the modal so that the user input can be compared with it. If button's data-buttonid is the same as the id of the question, append contents of the question div to the modal and show it
   $('#myModal').on('show.bs.modal', function (e) {
+    chances = 1;
     var $button = $(e.relatedTarget);
-    var $button_class = $($button).attr('class')
-    
+    var $button_class = $($button).attr('class');
+    // var $hidden_answer = $(this).parents('.whole_question').children('.individual_question').children('.hidden_answer').text();
+     var $hidden_answer = $(this).parents('.whole_question').children('.individual_question').children('.input').children('.hidden_answer').text();
+
     if ($button_class === 'has_id', 'btn', 'btn-primary', 'btn-lg'){ 
       var $button_id = $(e.relatedTarget).data("buttonid");
       var $container_id = parseInt($($button).parent().parent('.whole_question').attr('id'), 10);
-      var $question = $button.parent().parent().children('.individual_question')  
+      var $question = $button.parent().parent().children('.individual_question'); 
     }
     if ($($button_id === $container_id)){
       $('.modal-body').html($question.html());
+      
     }
-
-  //   $('.modal-title').text($('.category_title').text());
-
-  // var $title = $('.category_title').data("catid");
-  //   if ($title === $('.individual_question_id').data("catid")){
-  //     console.log("categories match!");
-  //     $('.modal-title').text($('.category_title').text());
-  //   } else {
-  //     console.log("categories don't match!");
-  //   }
+  // change the class of the clicked-on button to change styling and gray out the clicked button
+  $button.removeClass('btn-primary');
+  $button.addClass('btn-default');
+  $('#reveal_answer').empty();
   });
 
 
-  // reveal_question
-  // if($('.individual_question').is (":hidden")){ 
-  //   $($(this).children()).slideDown();
-  //   $hidden_answer.hide();
-  // } else if($('.individual_question').is (":visible")){ 
-  //   $($(this).children()).slideUp();
-  // }  
-
-
-
-
-$('#myModal').on('show.bs.modal', function (e) {
-  var $button = $(e.relatedTarget);
-  console.log("e.relatedTarget: " + $button);
-
-  $button.removeClass('btn-primary');
-  $button.addClass('btn-default');
-})
-
-
 $('.whole_question').on("click", ".submit_answer_button", function() {
+    $(this).parents('.modal-body').children('.input').children('.give_up_close').addClass('already_answered').text("close");
+    var $hidden_answer = $(this).parents('.modal-body').children('.input').children('.hidden_answer');
+    // var $hidden_answer = $(this).parents('.modal-body').children('.hidden_answer');
+    var $value = parseInt($(this).parents('.modal-body').children('.question_value').text(), 10);
 
-  var $hidden_answer = $(this).parents('.modal-body').children('.hidden_answer');
-  var $value = parseInt($(this).parents('.modal-body').children('.question_value').text(), 10);
+    var $user_answer = $(this).parent().parent().parent().find('input');
+    var $individual_question = $(this).parents('.modal-body').children('h2');
 
-  var $user_answer = $(this).parent().parent().parent().find('input');
-  var $individual_question = $(this).parents('.modal-body').children('h2');
 
-$('.user_answer').keydown(function(event){
-    if(event.keyCode == 13){
-      console.log("Enter key pressed");
-        // $(this).siblings('a').children('submit_answer_button').trigger('click');
-    }
-});
+  $('.user_answer').keydown(function(event){
+      if(event.keyCode == 13){
+        console.log("Enter key pressed");
+          // $(this).siblings('a').children('submit_answer_button').trigger('click');
+      }
+  });
 
   var hidden_answer = $.trim($hidden_answer.text().toLowerCase());
   console.log("THE QUESTION IS " + $individual_question.text());
@@ -128,42 +134,54 @@ function right_answer(){
   $('#reveal_answer').show();
 }
 
-
 // ------------------------------------------------------------
 
   if(user_answer === ""){
    console.log("Come on, at least guess")
-   $('#reveal_answer').append('<p>Come on, take a guess</p></br>').addClass('reveal_answer_wrong')
+   $(this).parents('.modal-body').children('.input').children('.give_up_close').text("I give up");
+   $('#reveal_answer').html('<p>Come on, take a guess</p></br>').addClass('reveal_answer_wrong');
     $('#reveal_answer').show();
+      // $('.modal-body').text('#reveal_answer');
+      chances = 1;
   } 
   //----- if user answer matches hidden answer  -----
   else if(user_answer.localeCompare(hidden_answer) === 0){
     right_answer();
     $(this).parent().hide()
+    $('#reveal_answer').show();
+      $(".hidden_answer").replace("_ ", user_answer);
+
   } else if(hidden_answer.indexOf(user_answer) >= 0){
     right_answer();
     $(this).parent().hide()
 
-    //------ if user answer does not match hidden answer ------
+    //------ if user answer does not match hidden answer (answer is wrong) ------
   } else if(user_answer != hidden_answer || user_answer.localeCompare(hidden_answer != 0 || hidden_answer.indexOf(user_answer) > 0)) {
     console.log("WRONG!!");
 
+      if(chances === 1){
+        $(this).text("Sorry, try again!");
+        $(this).parents('.input').children('.give_up_close').text("I give up");
+        // $(this).parents('.input').children('.user_answer').children().children('.hint_button').show();
+
+        chances --;
+        } else if(chances === 0){
+      
     var dollar_amount = $value;
     game_score = game_score - parseInt(dollar_amount, 10);
     $('#game_score').text(game_score);
     console.log(game_score);
     counter--;
     $(this).parent().hide()
-
       
     $('#which_round').text(counter + " ");
     console.log($('#which_round').text());
   
-    $('#reveal_answer').append('<p>' + user_answer.toUpperCase() + '</br>Sorry, wrong answer. You\'ve lost $' + $value + '.</br> The right answer was "' + hidden_answer.toUpperCase() + '".</p></br>').addClass('reveal_answer_wrong')
+    $('#reveal_answer').append('<p>' + user_answer.toUpperCase() + '</br>Sorry, wrong answer. You\'ve lost $' + $value + '.</br> The right answer was "' + hidden_answer.toUpperCase() + '".</p></br>').addClass('reveal_answer_wrong');
     $('#reveal_answer').show();
   
 
-
+    // when counter reaches 0, change text of status h2, reset counter to 5 and show the new_game button. ajax call to post scores and create new game in the db
       if(counter === 0){
         alert("GAME OVER");
         console.log("GAME OVER!");
@@ -180,13 +198,20 @@ function right_answer(){
         }).done(function(data){
           console.log("DATA :" + data)
         })
-      } else if (counter != 0){
-        console.log("YOU WON")
+      // } 
       }
     }
+  }
 })
 
-// ajax call to server to get new batch of questions
+// when a button is clicked, disable button to keep track of which questions have already been seen
+$('#myModal').on('shown.bs.modal', function (e) {
+  var $clicked = $(e.relatedTarget); 
+  $clicked.prop('disabled', true);
+
+});
+
+// ajax call to server to get new batch of questions. Empty the page and then append the results of the ajax call
   $('#new_game').on('click', function() {
     // $('#new_game').hide();
     console.log("new game button clicked");
@@ -197,21 +222,8 @@ function right_answer(){
         console.log("SUCCESS");
         $('body').empty();
         $('body').append(data);
-      })
-      
+      })  
   })
 
-
-});
-  // tell user whether answer is right or wrong.
-  //   if right
-        // add value to running score
-  //     reveal correct answer, colored in green
-  //   if wrong
-  //      subtract value from running score
-  //     counter decreases by 1
-  //     reveal correct answer, colored in red
-  //        and counter = 0:
-        //  display game over
-            // display running score and post to post to database
-            // display total score and post to database
+  // close of document.ready
+  });
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,7 +11,9 @@ class GamesController < ApplicationController
 	  @response3 = HTTParty.get("http://jservice.io/api/category?id=#{category_id3}")
 	  
 	  @contestant = Contestant.all
-	  # @contestant = Contestant.find(params[:id])
+	  @contestant = Contestant.find(params[:id])
+		@id = params["contestant_id"]
+
 
 # binding.pry
 
@@ -25,15 +27,67 @@ class GamesController < ApplicationController
 		# binding.pry
 	end
 
+	
 	def show
-		category_id = rand(0..18000)
-	  @response = HTTParty.get("http://jservice.io/api/category?id=#{category_id}")
+
+		def api_call(category_id)
+		  @response = HTTParty.get("http://jservice.io/api/category?id=#{category_id}")
+		end
 		
-		category_id2 = rand(0..18000)
-	  @response2 = HTTParty.get("http://jservice.io/api/category?id=#{category_id2}")
+		api_call(rand(0..18000))
 		
-		category_id3 = rand(0..18000)
-	  @response3 = HTTParty.get("http://jservice.io/api/category?id=#{category_id3}")
+		# binding.pry
+		while @nil == true do
+			first_five = @response["clues"].first(5)
+			first_five.each do |question|
+				if question["value"] == nil
+					question["value"] = "NIL"
+				else 		
+					question["value"] = question["value"]
+				end
+			end
+		end	
+
+
+		def api_call2(category_id)
+		  @response2 = HTTParty.get("http://jservice.io/api/category?id=#{category_id}")
+		end
+		
+		api_call2(rand(0..18000))
+
+		@nil2 = true
+		
+		while @nil2 == true do
+			first_five = @response2["clues"].first(5)
+			first_five.each do |question|
+				if question["value"] != nil
+					@nil2 = false
+				else 		
+					api_call2(rand(0..18000))
+				end
+			end
+		end	
+	
+
+		def api_call3(category_id)
+		  @response3 = HTTParty.get("http://jservice.io/api/category?id=#{category_id}")
+		end
+		
+		api_call3(rand(0..18000))
+
+		@nil3 = true
+		
+		while @nil3 == true do
+			first_five = @response3["clues"].first(5)
+			first_five.each do |question|
+				if question["value"] != nil
+					@nil3 = false
+				else 		
+					api_call3(rand(0..18000))
+				end
+			end
+		end	
+
 
  		# @contestant = Contestant.find(params["id"])
 	  @contestant = Contestant.find(params[:id])
@@ -41,6 +95,6 @@ class GamesController < ApplicationController
 		@game = Game.all
 	# binding.pry
 
+	
 	end
-
 end
