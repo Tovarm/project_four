7da5b17569dab4985c9c28ca1d2b0b9380f92c28
Tovarm <tovamosk@gmail.com>
2015-07-29 13:51:12 -0400

    config/routes fixed file- root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'games#new'
+  root 'contestants#new'
 
   resources :games
   resources :contestants
