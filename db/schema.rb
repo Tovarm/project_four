d2c71628f23a6721a291d97e5556f414adfadcbf
Tovarm <tovamosk@gmail.com>
2015-08-03 16:40:22 -0400

    add created_at to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803202915) do
+ActiveRecord::Schema.define(version: 20150803203848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,10 @@ ActiveRecord::Schema.define(version: 20150803202915) do
   end
 
   create_table "games", force: true do |t|
-    t.integer "contestant_id"
-    t.string  "username"
-    t.integer "game_score"
+    t.integer  "contestant_id"
+    t.string   "username"
+    t.integer  "game_score"
+    t.datetime "created_at"
   end
 
 end
